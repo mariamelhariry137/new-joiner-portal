b7e3fbbe471498729bb3bac3be6187af8535cd7f
Youssefessamaly <essamyoussef513@gmail.com>
2026-08-18 15:41:06 +0300

    ALl services working

--- auth-service/src/main/java/com/newjoinerportal/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/newjoinerportal/auth/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                                 "/api/auth/register",
                                 "/api/auth/login"
                         ).permitAll()
+
                         .anyRequest().authenticated()
                 );
 
--- auth-service/src/main/java/com/newjoinerportal/auth/controller/CurrentUserController.java
+++ auth-service/src/main/java/com/newjoinerportal/auth/controller/CurrentUserController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 public class CurrentUserController {
