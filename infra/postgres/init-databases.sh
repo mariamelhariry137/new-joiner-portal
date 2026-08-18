f786efbee6d21170d445586635bf5cfb99411523
romiiali <ra7ma.anwar@hotmail.com>
2026-08-18 23:39:23 +0300

    completed editing of gateway files

--- infra/postgres/init-databases.sh
+++ infra/postgres/init-databases.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
-    CREATE DATABASE identity_db;
+    CREATE DATABASE new_joiner_auth;
     CREATE DATABASE onboarding_db;
     CREATE DATABASE content_db;
 EOSQL
